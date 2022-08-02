204ac9ced8edc4894d25566f73467d50533d7b4e
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-08-01 22:49:50 -0300

    back

--- models/review.js
+++ models/review.js
@@ -1,17 +1,16 @@
-
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const ReviewSchema = new Schema({
-  body: String,
-  username: String,
-  rating: Number,
-}, {
-  toJSON: {
-    virtuals: true,
+const ReviewSchema = new Schema(
+  {
+    body: String,
+    username: String,
+    rating: Number,
   },
-});
-
-module.exports = mongoose.model('Review', ReviewSchema);
-
-
-
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+console.log("back");
+module.exports = mongoose.model("Review", ReviewSchema);
